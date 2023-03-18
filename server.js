0204c7d9c00f2b8bb802fdafe0c50d57207e97c4
bablyaisha <bablyaisha786@gmail.com>
2023-03-19 01:35:04 +0530

    Ready to deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from './config/db.js';
-import authRoute from './routers/authRoute.js'
-import productRoutes from './routers/productRoutes.js'
-import cors from 'cors'
-import categoryRoutes from './routers/categoryRoutes.js'
+import authRoute from './routers/authRoute.js';
+import productRoutes from './routers/productRoutes.js';
+import cors from 'cors';
+import categoryRoutes from './routers/categoryRoutes.js';
+import path from 'path';
 
 //configure env
 dotenv.config();
@@ -20,18 +21,18 @@ const app= express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/category', categoryRoutes);
-app.use('/api/v1/product', productRoutes)
+app.use('/api/v1/product', productRoutes);
+
 
 //rest api
-app.get('/', (req,res)=>{
-    res.send(
-       "<h1>Welcome to Kitaabji!</h1>"
-    )
-})
+app.use('*', function(req,res){
+       res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
 
 //port
 const PORT =process.env.PORT || 8080 ;
